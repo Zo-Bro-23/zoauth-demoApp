f15a28fa7479fd8a911e3a1daac450e3df550eef
Zo-Bro-23 <zohan.subhash@gmail.com>
2021-09-15 12:51:32 +0530

    Commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ZoAuth Demo app
 This is a demo app for the ZoAuth npm package...
 
-Go to [https://demo.zoauth.tk](https://demo.zoauth.tk) to see this demo app in action. Feel free to clone this project and be sure to join in the discussion with your own tips and tricks!
+Go to [https://demo.zoauth.zohan.tech](https://demo.zoauth.zohan.tech) to see this demo app in action. Feel free to clone this project and be sure to join in the discussion with your own tips and tricks!
--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const app = zoauth.expressApp
 
 zoauth.setCredentials({
     google: {
-        redirect_uri: "https://demo.zoauth.tk/callback/google"
+        redirect_uri: "https://demo.zoauth.zohan.tech/callback/google"
     },
     discord: {
-        redirect_uri: "https://demo.zoauth.tk/callback/discord"
+        redirect_uri: "https://demo.zoauth.zohan.tech/callback/discord"
     },
     facebook: {
-        redirect_uri: "https://demo.zoauth.tk/callback/facebook/"
+        redirect_uri: "https://demo.zoauth.zohan.tech/callback/facebook/"
     },
     microsoft: {
-        redirect_uri: "https://demo.zoauth.tk/callback/microsoft/"
+        redirect_uri: "https://demo.zoauth.zohan.tech/callback/microsoft/"
     },
     github: {
-        redirect_uri: "https://demo.zoauth.tk/callback/github"
+        redirect_uri: "https://demo.zoauth.zohan.tech/callback/github"
     },
     amazon: {
-        redirect_uri: "https://demo.zoauth.tk/callback/amazon"
+        redirect_uri: "https://demo.zoauth.zohan.tech/callback/amazon"
     }
 })
 zoauth.runAuthServer(r => {
@@ -26,14 +26,14 @@ zoauth.runAuthServer(r => {
     zoauth.getDetails(r.company, r.data)
         .then(res => {
             if (r.company == 'github') {
-                r.responseObject.redirect(`https://demo.zoauth.tk?${r.company}=${res.login}`)
+                r.responseObject.redirect(`https://demo.zoauth.zohan.tech?${r.company}=${res.login}`)
             } else if (r.company == 'discord') {
-                r.responseObject.redirect(`https://demo.zoauth.tk?${r.company}=${res.user.username}`)
+                r.responseObject.redirect(`https://demo.zoauth.zohan.tech?${r.company}=${res.user.username}`)
             } else {
-                r.responseObject.redirect(`https://demo.zoauth.tk?${r.company}=${res.name}`)
+                r.responseObject.redirect(`https://demo.zoauth.zohan.tech?${r.company}=${res.name}`)
             }
         }, err => {
-            err.responseObject.redirect(`https://demo.zoauth.tk/${r.company}=${'An error occured. Please try again.'}`)
+            err.responseObject.redirect(`https://demo.zoauth.zohan.tech/${r.company}=${'An error occured. Please try again.'}`)
         })
 })
 
