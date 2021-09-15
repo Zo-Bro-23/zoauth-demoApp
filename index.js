972f808e467115aa183d0f670ff82afd3030fbea
Zo-Bro-23 <zohan.subhash@gmail.com>
2021-09-15 16:24:03 +0530

    Debugging

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ zoauth.runAuthServer(r => {
             } else if (r.company == 'discord') {
                 r.responseObject.redirect(`https://demo.zoauth.zohan.tech?${r.company}=${res.user.username}`)
             } else {
-                console.log(`https://demo.zoauth.zohan.tech?${r.company}=${res.name}`)
+                r.responseObject.send(`https://demo.zoauth.zohan.tech?${r.company}=${res.name}`)
                 r.responseObject.redirect(`https://demo.zoauth.zohan.tech?${r.company}=${res.name}`)
             }
         }, err => {
